b5743ee64cf4bb16b332de121b4fda460cc08d6e
rajcdlmec35 <62494029+rajcdlmec35@users.noreply.github.com>
2020-04-08 02:47:46 +0530

    Update execBuildOrDeploy.groovy

--- vars/execBuildOrDeploy.groovy
+++ vars/execBuildOrDeploy.groovy
@@ -1,6 +1,8 @@
 
 
 def call(String command) {
+	def buildTag = env.BUILD_NUMBER
+        def branchNamePlaceholder = ''
 	if(!(env.BRANCH_NAME =~ /^v([0-9]+\.[0-9]+\.[0-9]+)/)) {
         buildTag = "${env.BRANCH_NAME}-${env.BUILD_NUMBER}"
         branchNamePlaceholder = "-${env.BRANCH_NAME}"
